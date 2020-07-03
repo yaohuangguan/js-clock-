91fd28f77f5a5cbb01ef40eb61a243c0c39aa426
yaohuangguan <yaob@miamioh.edu>
2020-07-03 20:15:28 +0800

    int

---
+++ index.js
@@ -0,0 +1,16 @@
+const deg = 6;
+const hr = document.getElementById("hr");
+const mn = document.getElementById("mn");
+const sc = document.getElementById("sc");
+const updateTime = () => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+  window.requestAnimationFrame(updateTime);
+};
+updateTime();
